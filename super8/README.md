fce68513334067e08c64a097f0d18fe56bff457b
Philipp Staender <pstaender@mailbox.org>
2022-07-05 19:36:16 +0200

    adds super 8 projector

---
+++ super8/README.md
@@ -0,0 +1,8 @@
+# Super 8 Projector
+
+- Grain and dust video: https://www.shutterstock.com/blog/how-to-use-free-film-grain-overlays?utm_source=youtube&utm_medium=blog&utm_content=Using-Film-Grain&utm_campaign=2-2019-sstk-blog
+- Alternate grain and dust video: https://vimeo.com/29185333
+
+## License
+
+MIT
---
+++ super8/super8.js
@@ -0,0 +1,143 @@
+const video = document.getElementById("video");
+const videoGrain = document.getElementById("video-grain");
+const videoScratches = document.getElementById("video-scratches");
+const projectorSound = document.getElementById("projector-sound");
+const videoInput = document.getElementById("video-input");
+
+let blurRadius = 2;
+let saturate = 1.25;
+let bwContrast = 1.1;
+let colorContrast = 1;
+
+projectorSound.volume = 0.5;
+const timeoutForSkipping = 250;
+
+const toggleVideoPlayback = (isPlaying) => {
+  if (isPlaying) {
+    setTimeout(() => {
+      videoGrain.playbackRate = 1;
+      videoScratches.playbackRate = 1;
+      if (!video.pause) {
+        projectorSound.currentTime = 0;
+      }
+    }, timeoutForSkipping);
+    videoGrain.play();
+    videoScratches.play();
+    projectorSound.play();
+  } else {
+    // play ending sound
+    setTimeout(() => {
+      if (video.paused) {
+        videoGrain.playbackRate = 0.25;
+        videoScratches.playbackRate = 0.25;
+        setTimeout(() => {
+          videoGrain.pause();
+          videoScratches.pause();
+        }, 100);
+
+        projectorSound.currentTime = 1211.25;
+      }
+    }, timeoutForSkipping);
+  }
+};
+
+video.addEventListener("pause", (event) => {
+  toggleVideoPlayback(event.type === "play");
+  if (video.currentTime >= video.duration) {
+    setTimeout(() => video.classList.add("finished"), 250);
+  }
+});
+video.addEventListener("play", (event) => {
+  document.querySelector("body").classList.remove("loading");
+  video.classList.remove("finished");
+  toggleVideoPlayback(event.type === "play");
+});
+
+const assignBackdropFilters = () => {
+  document.getElementById(
+    "blur-filter"
+  ).style.backdropFilter = `blur(${blurRadius}px) saturate(${saturate})`;
+  document.getElementById("bw-filter").style.backdropFilter = `brightness(${
+    bwContrast * 0.95
+  }) contrast(${bwContrast}) grayscale(1)`;
+};
+
+document.addEventListener("keyup", (event) => {
+  if (event.key === "b") {
+    document.querySelector("body").classList.toggle("bw-film");
+  }
+  if (event.key === "+" || event.key === "-") {
+    blurRadius = blurRadius + Number(event.key + "1");
+    if (blurRadius < 0) {
+      blurRadius = 0;
+    }
+  }
+  if (event.key === "[") {
+    bwContrast = bwContrast - 0.05;
+    if (bwContrast < 0) {
+      bwContrast = 0;
+    }
+  }
+  if (event.key === "]") {
+    bwContrast = bwContrast + 0.05;
+    if (bwContrast > 2) {
+      bwContrast = 2;
+    }
+  }
+  if (event.key === "s") {
+    projectorSound.muted = !projectorSound.muted;
+  }
+  if (event.key === "m") {
+    video.muted = !video.muted;
+  }
+  if (event.key === "c") {
+    video.controls = !video.controls;
+  }
+  if (event.key === "l") {
+    video.loop = !video.loop;
+  }
+  if (event.key === "h") {
+    document.querySelector("body").classList.toggle("show-help");
+  }
+  assignBackdropFilters();
+});
+
+const onMediaLoaded = (ev) => {
+  if (ev.target.readyState >= 3) {
+    ev.target.classList.remove("unloaded");
+  }
+
+  if (!document.querySelector("video.unloaded")) {
+    document.querySelector("body").classList.remove("loading");
+    setTimeout(() => {
+      document.querySelector("body").classList.add("hide-help");
+    }, 500);
+  }
+};
+document.querySelectorAll("video").forEach((e) => {
+  e.classList.add("unloaded");
+  // e.addEventListener("loadeddata", onMediaLoaded);
+  e.addEventListener("suspend", onMediaLoaded);
+});
+
+document.getElementById("video-input").addEventListener("drop", (event) => {
+  setTimeout(() => {
+    video.src = URL.createObjectURL(videoInput.files[0]);
+    video.play();
+  }, 500);
+  document.querySelector("body").classList.remove("dragover");
+});
+
+document
+  .querySelector(".help-box")
+  .addEventListener("click", () =>
+    document.querySelector("body").classList.remove("show-help")
+  );
+
+let showHelp = localStorage.getItem("showedHelpInitial");
+if (!showHelp) {
+  document.querySelector("body").classList.add("show-help");
+  localStorage.setItem("showedHelpInitial", "showed");
+}
+
+assignBackdropFilters();
